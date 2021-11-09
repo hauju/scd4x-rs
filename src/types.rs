fa29e0c14573d30fd9abea297088148de489fa53
hauju <hauke.jung@outlook.de>
2021-11-09 20:16:19 +0100

    Add derive to structs

--- src/types.rs
+++ src/types.rs
@@ -1,11 +1,14 @@
 //! Types for the SCD4x sensor.
+use serde::{Deserialize, Serialize};
 
+#[derive(Serialize, Deserialize, Debug)]
 pub struct SensorData {
     pub co2: u16,
     pub temperature: f32,
     pub humidity: f32,
 }
 
+#[derive(Serialize, Deserialize, Debug)]
 pub struct RawSensorData {
     pub co2: u16,
     pub temperature: u16,

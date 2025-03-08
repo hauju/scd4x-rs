8d2395b882f497630329080a1f937632c61b8449
hauju <hauke.jung@outlook.de>
2025-03-08 21:19:26 +0100

    Update src/lib.rs
    Co-authored-by: Tamme Dittrich <td-github@orangecar.org>

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 //! This library provides an embedded `no_std` driver for the [Sensirion SCD4x series](https://www.sensirion.com/de/umweltsensoren/evaluationskit-sek-environmental-sensing/evaluationskit-sek-scd41/).
-//! This driver was built using [`embedded-hal``](https://docs.rs/embedded-hal/) traits.
+//! This driver was built using [`embedded-hal`](https://docs.rs/embedded-hal/) traits.
 //! The implementaion are based on [embedded-i2c-scd4x](https://github.com/Sensirion/embedded-i2c-scd4x) and [sgpc3-rs](https://github.com/mjaakkol/sgpc3-rs).
 //!
 //! ## `embedded-hal-async` Support
